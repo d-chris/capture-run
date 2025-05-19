c2c410e18bde8066c663245e889c0398753e9fd0
Christoph Dörrer <d-chris@web.de>
2025-05-19 18:03:28 +0200

    add test fixtures for encoding and command parameters

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import pytest as p
+import sys
+
+
+@p.fixture(
+    params=[None, "utf-8", "ansi", "cp850", "cp1252"],
+    ids=lambda x: f"enc={x}",
+)
+def encoding(request: p.FixtureRequest) -> str | None:
+    """Fixture to provide encoding parameter."""
+    return request.param
+
+
+@p.fixture(
+    params=[
+        ["echo", "ok"],
+        ["echo", "nok", ";", "exit", "1"],
+        ["echo", "☺ bright", ";", "exit", "0"],
+        ["echo", "\u263b dark", ";", "exit", "1"],  # "☻"
+        ["echo", "\U0001f680 rocket"],  # 🚀
+    ],
+    ids=[
+        "echo=ok",
+        "echo=nok",
+        "echo=bright",
+        "echo=dark",
+        "echo=rocket",
+    ],
+)
+def commands(request: p.FixtureRequest) -> list[str]:
+    if sys.platform == "win32":
+        return ["&" if x == ";" else x for x in request.param]
+    else:
+        return request.param
---
+++ tests/test_exec.py
@@ -0,0 +1,50 @@
+import pytest as p
+import subprocess
+from capture import run
+
+
+@p.mark.parametrize("text", [True, False], ids=lambda x: f"txt={x}")
+def test_run_exec(
+    capfdbinary: p.CaptureFixture,
+    commands: list[str],
+    encoding: str | None,
+    text: bool,
+) -> None:
+
+    kwargs = {
+        "args": commands,
+        "text": text,
+        "encoding": encoding,
+    }
+
+    a = subprocess.run(
+        capture_output=True, **kwargs
+    )  # type: subprocess.CompletedProcess
+    asys = capfdbinary.readouterr()
+
+    c = subprocess.run(
+        capture_output=False, **kwargs
+    )  # type: subprocess.CompletedProcess
+    csys = capfdbinary.readouterr()
+
+    b = run(capture_output=False, **kwargs)  # type: subprocess.CompletedProcess
+    bsys = capfdbinary.readouterr()
+
+    d = run(capture_output=True, **kwargs)  # type: subprocess.CompletedProcess
+    dsys = capfdbinary.readouterr()
+
+    assert a.args == b.args == c.args == d.args, "missmatch in arguments"
+    assert (
+        a.returncode == b.returncode == c.returncode == d.returncode
+    ), "missmatch in returncode"
+
+    assert asys.out == asys.err == b"", "subprocess.run() should be capture output"
+
+    assert a.stdout == b.stdout == d.stdout, f"missmatch in captured stdout {encoding=}"
+    assert a.stderr == b.stderr == d.stderr, f"missmatch in captured stderr {encoding=}"
+
+    assert csys.out == bsys.out, f"missmatch in console output {encoding=}"
+    assert csys.err == bsys.err, f"missmatch in console error {encoding=}"
+
+    assert asys.out == dsys.out, f"missmatch in captured console output {encoding=}"
+    assert asys.err == dsys.err, f"missmatch in captured console error {encoding=}"
